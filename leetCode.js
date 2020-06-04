f8004cec0fcc0f4c441b76426186774858e999f7
Yangfsssss <376476191@qq.com>
2020-06-04 17:48:55 +0800

    26 added

--- leetCode.js
+++ leetCode.js
@@ -172,9 +172,25 @@
 
 
 // leetCode 21
-const mergeTwoLists = (l1, l2) => {
+// const mergeTwoLists = (l1, l2) => {
     
-}
+// }
+
+
+// leetCode 26
+// const removeDuplicates = nums => {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] === nums[j]) {
+//                 nums.splice(j, 1)
+//                 j--
+//             }
+//         }
+//     }
+//     return nums.length
+// }
+// console.log(removeDuplicates([1, 1, 2]));
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
 
 
 
